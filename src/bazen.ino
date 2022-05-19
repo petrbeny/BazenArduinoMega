7103f874ce36f507ca3d5dd2b9851af463cf17e7
Petr Beneš <petr.benes@actumdigital.com>
2022-05-19 08:17:54 +0200

    Added pH sensor

--- src/bazen.ino
+++ src/bazen.ino
@@ -28,6 +28,7 @@ void setup()
 
 void loop()
 {
+  // --- DHT senzor -------------------------------------------------------------------------------------------
   // nacteni teploty a vlhkosti s DHT senzoru
   float tep = mojeDHT.readTemperature();
   float vlh = mojeDHT.readHumidity();
@@ -47,11 +48,53 @@ void loop()
     // client.publish("/house/downstairs/komora/thermometer/humidity", String(vlh, 3).c_str());
   }
 
+  // --- Dallas senzor -----------------------------------------------------------------------------------------
   // načtení informací ze všech připojených čidel na daném pinu
   senzoryDS.requestTemperatures();
   Print("Teplota bazenu", senzoryDS.getTempCByIndex(0), "°C");
 
-  delay(20000);
+  // --- PH senzor ---------------------------------------------------------------------------------------------
+  // vytvoření pomocných proměnných
+  int pole[10];
+  int zaloha;
+  unsigned long int prumerVysl = 0;
+  // načtení deseti vzorků po 10 ms do pole
+  for (int i = 0; i < 10; i++)
+  {
+    pole[i] = analogRead(PH_PIN);
+    delay(10);
+  }
+  // seřazení členů pole naměřených výsledků podle velikosti
+  for (int i = 0; i < 9; i++)
+  {
+    for (int j = i + 1; j < 10; j++)
+    {
+      if (pole[i] > pole[j])
+      {
+        zaloha = pole[i];
+        pole[i] = pole[j];
+        pole[j] = zaloha;
+      }
+    }
+  }
+  // uložení 2. až 8. výsledku do
+  // proměnné, z které se vypočte průměr
+  // (vynechání dvou členů pole na začátku
+  // a konci pro lepší přesnost)
+  for (int i = 2; i < 8; i++)
+  {
+    prumerVysl += pole[i];
+  }
+  // výpočet hodnoty pH z průměru
+  // měření a přepočet na rozsah 0-14 pH
+  float prumerPH = (float)prumerVysl * 5.0 / 1024 / 6;
+  float vyslednePH = -5.70 * prumerPH + 21.34;
+  // vytištění výsledků po sériové lince
+  // Print("PH bazenu", vyslednePH, "pH");
+  Serial.print("Namerene pH: ");
+  Serial.println(vyslednePH);
+
+  delay(900);
 }
 
 void Print(String source, int value, String unit)
